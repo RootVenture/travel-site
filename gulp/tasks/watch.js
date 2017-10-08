b150f5e2a1252b27362aed6d31cbacd77f2ce553
Ray W <theunholydonuts@gmail.com>
2017-10-08 12:12:19 -0400

    Organized gulpfile.js

---
+++ gulp/tasks/watch.js
@@ -0,0 +1,27 @@
+var gulp = require("gulp"),
+  watch = require("gulp-watch"),
+  browserSync = require("browser-sync").create();
+
+gulp.task("watch", function() {
+  browserSync.init({
+    notify: false,
+    server: {
+      baseDir: "app"
+    }
+  });
+
+  watch("./app/index.html", function() {
+    browserSync.reload();
+  });
+
+  // task runner to watch changes on any files in our styles folder with the css ini
+  // if detected, will cause async update of webpage css 'cssInject'
+  watch("./app/assets/styles/**/*.css", function() {
+    gulp.start("cssInject");
+  });
+});
+
+// Note: 2nd argument is the 'dependency' that needs to be run prior to the callback function
+gulp.task("cssInject", ["styles"], function() {
+  return gulp.src("./app/temp/styles/styles.css").pipe(browserSync.stream());
+});
