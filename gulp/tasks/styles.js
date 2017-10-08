6690d5de38438be37879faf63b59e046070af1f5
Ray W <theunholydonuts@gmail.com>
2017-10-08 14:23:36 -0400

    Added responsive images to the large-hero-image

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -3,11 +3,18 @@ var gulp = require("gulp"),
   autoprefixer = require("autoprefixer"),
   cssvars = require("postcss-simple-vars"),
   nested = require("postcss-nested"),
-  cssImport = require("postcss-import");
+  cssImport = require("postcss-import"),
+  mixins = require("postcss-mixins");
 
 gulp.task("styles", function() {
   return gulp
     .src("./app/assets/styles/styles.css")
-    .pipe(postcss([cssImport, cssvars, nested, autoprefixer]))
+    .pipe(postcss([cssImport, mixins, cssvars, nested, autoprefixer]))
+    .on("error", function(error) {
+      // to string makes the error more readable
+      console.log(error.toString());
+      // for this stream, tell gulp to end the task
+      this.emit("end");
+    })
     .pipe(gulp.dest("./app/temp/styles"));
 });
