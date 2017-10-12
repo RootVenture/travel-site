7c877257d23fd1774c20f379eb434eca3b389c57
Ray W <theunholydonuts@gmail.com>
2017-10-11 21:24:59 -0400

    Completed automatic sprites with gulp

--- gulp/tasks/sprites.js
+++ gulp/tasks/sprites.js
@@ -1,9 +1,13 @@
 var gulp = require("gulp"),
-  svgSprite = require("gulp-svg-sprite");
+  svgSprite = require("gulp-svg-sprite"),
+  rename = require("gulp-rename"),
+  del = require("del");
 
 var config = {
   mode: {
     css: {
+      // updates the filename (removes .css)
+      sprite: "sprite.svg",
       // options for the svg package to create our css
       render: {
         css: {
@@ -14,10 +18,40 @@ var config = {
   }
 };
 
-gulp.task("createSprite", function() {
+gulp.task("beginClean", function() {
+  return del(["./app/temp/sprite", ".app/assets/images/sprites"]);
+});
+
+gulp.task("createSprite", ["beginClean"], function() {
   // gulp src always needs to return something
   return gulp
     .src("./app/assets/images/icons/**/*.svg")
     .pipe(svgSprite(config))
     .pipe(gulp.dest("./app/temp/sprite/"));
 });
+
+gulp.task("createSpriteGraphic", ["createSprite"], function() {
+  return gulp
+    .src("./app/temp/sprite/css/**/*.svg")
+    .pipe(gulp.dest("./app/assets/images/sprites"));
+});
+
+// note the dependency to createSprite
+gulp.task("copySpriteCSS", ["createSprite"], function() {
+  return gulp
+    .src("./app/temp/sprite/css/*.css")
+    .pipe(rename("_sprite.css"))
+    .pipe(gulp.dest("./app/assets/styles/modules"));
+});
+
+gulp.task("endClean", ["createSpriteGraphic", "copySpriteCSS"], function() {
+  return del(["./app/temp/sprite"]);
+});
+
+gulp.task("icons", [
+  "beginClean",
+  "createSprite",
+  "createSpriteGraphic",
+  "copySpriteCSS",
+  "endClean"
+]);
